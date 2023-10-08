0485e4cc6453f1c0ab2ca6a33a520d3288bd31dc
prjmanthey <80652353+prjmanthey@users.noreply.github.com>
2023-10-08 02:25:24 -0400

    Update Documentation

--- R/lusongtest.R
+++ R/lusongtest.R
@@ -1,6 +1,6 @@
 #' @title lusongtest: Package for Performing a One-Sample Lu-Song Test on Variance
 #'
-#' @description An R package for performing Lu-Song tests. This is a nonparametric hypothesis test that performs a one-sample test of variance. The test is based on the methodology described by Yuan-yuan Lu and Li-xin Song in their paper U-Statistics Testing Method in Testing Variance Differences of a Single Population.
+#' @description An R package for performing Lu-Song tests. This is a nonparametric hypothesis test that performs a one-sample test of variance. The test is based on the methodology described by Yuan-yuan Lu and Li-xin Song in their paper "*U*-Statistics Testing Method in Testing Variance Differences of a Single Population".
 #'
 #'
 #' @docType package
